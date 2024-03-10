d14d965627d79ed5797531568c8a4cf6fd51059e
sudeshpariyar <sudesh.bullet@gmail.com>
2024-03-10 23:38:30 +1100

    fix footer and its links

--- client/src/Layout/Footer/Footer.tsx
+++ client/src/Layout/Footer/Footer.tsx
@@ -3,8 +3,15 @@ import "./Footer.css";
 import ComponentWrapper from "../../components/ComponentWrapper/ComponentWrapper";
 import CustomHeading from "../../components/CustomHeading/CustomHeading";
 import { FaRegCopyright } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const data = {
+    query: "",
+  };
+  const handleClick = (value: string) => {
+    data.query = value;
+  };
   return (
     <div className="footer-wrapper">
       <ComponentWrapper>
@@ -19,32 +26,123 @@ const Footer = () => {
           <div className="footer-right-component">
             <div>
               <CustomHeading headingLevel="h4">World</CustomHeading>
-              <CustomHeading headingLevel="h6">Politics</CustomHeading>
-              <CustomHeading headingLevel="h6">Health</CustomHeading>
-              <CustomHeading headingLevel="h6">Business</CustomHeading>
-              <CustomHeading headingLevel="h6">Tech</CustomHeading>
-              <CustomHeading headingLevel="h6">Entertainment</CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link to="/political">Politics</Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link to="/health">Health</Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link to="/business">Business</Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link to="/entertainment">Entertainment</Link>
+              </CustomHeading>
             </div>
             <div>
               <CustomHeading headingLevel="h4">Tech</CustomHeading>
-              <CustomHeading headingLevel="h6">Science</CustomHeading>
-              <CustomHeading headingLevel="h6">Magazines</CustomHeading>
-              <CustomHeading headingLevel="h6">Start up</CustomHeading>
-              <CustomHeading headingLevel="h6">Crypto</CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link to="/science">Science</Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link
+                  onClick={() => handleClick("magazines")}
+                  to="/search"
+                  state={data}
+                >
+                  Magazines
+                </Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link
+                  onClick={() => handleClick("start up")}
+                  to="/search"
+                  state={data}
+                >
+                  Start up
+                </Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link
+                  onClick={() => handleClick("crypto")}
+                  to="/search"
+                  state={data}
+                >
+                  Crypto
+                </Link>
+              </CustomHeading>
             </div>
             <div>
               <CustomHeading headingLevel="h4">Life</CustomHeading>
-              <CustomHeading headingLevel="h6">Food</CustomHeading>
-              <CustomHeading headingLevel="h6">Style</CustomHeading>
-              <CustomHeading headingLevel="h6">Sport</CustomHeading>
-              <CustomHeading headingLevel="h6">Movie</CustomHeading>
-              <CustomHeading headingLevel="h6">Music</CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link
+                  onClick={() => handleClick("food")}
+                  to="/search"
+                  state={data}
+                >
+                  Food
+                </Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link
+                  onClick={() => handleClick("Style")}
+                  to="/search"
+                  state={data}
+                >
+                  Style
+                </Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link to="/sports">Sports</Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link
+                  onClick={() => handleClick("Movie")}
+                  to="/search"
+                  state={data}
+                >
+                  Movie
+                </Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link
+                  onClick={() => handleClick("Music")}
+                  to="/search"
+                  state={data}
+                >
+                  Music
+                </Link>
+              </CustomHeading>
             </div>
             <div>
               <CustomHeading headingLevel="h4">Magezine</CustomHeading>
-              <CustomHeading headingLevel="h6">Fashion</CustomHeading>
-              <CustomHeading headingLevel="h6">Blogger</CustomHeading>
-              <CustomHeading headingLevel="h6">People</CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link
+                  onClick={() => handleClick("Fashion")}
+                  to="/search"
+                  state={data}
+                >
+                  Fashion
+                </Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link
+                  onClick={() => handleClick("Blogger")}
+                  to="/search"
+                  state={data}
+                >
+                  Blogger
+                </Link>
+              </CustomHeading>
+              <CustomHeading headingLevel="h6">
+                <Link
+                  onClick={() => handleClick("People")}
+                  to="/search"
+                  state={data}
+                >
+                  People
+                </Link>
+              </CustomHeading>
             </div>
             <div>
               <CustomHeading headingLevel="h4">Other</CustomHeading>
